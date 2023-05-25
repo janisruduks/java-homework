192f756a12e773c17090c0da93ee48d66f194d85
Jjanisruduks <janis.ruduks157@gmail.com>
2023-05-25 10:29:01 +0300

    For excercise 3 added user input

--- src/main/java/io/codelex/exceptions/practice/Exercise3.java
+++ src/main/java/io/codelex/exceptions/practice/Exercise3.java
@@ -3,11 +3,13 @@ package io.codelex.exceptions.practice;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Scanner;
 
 public class Exercise3 {
     public static void main(String[] args) throws IOException {
-        cat(new File("/Users/janis/Desktop/hello.txt"));
-        cat(new File("world"));
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter path to file e.g. '/Users/name/example.txt'");
+        cat(new File(in.next()));
     }
 
     public static void cat(File file) {
