779293db5528e0cf98f4f3d4f884cec929ade9fd
Jjanisruduks <janis.ruduks157@gmail.com>
2023-05-28 22:25:41 +0300

    You can do it future me!

--- src/main/java/io/codelex/collections/practice/WordCount.java
+++ src/main/java/io/codelex/collections/practice/WordCount.java
@@ -6,13 +6,40 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
 
 public class WordCount {
     private static final Charset charset = Charset.defaultCharset();
     private static final String file = "/collections/lear.txt";
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        final Path path = Paths.get(Histogram.class.getResource(file).toURI());
-        Files.readAllLines(path, charset);
+        final Path path = Paths.get(Objects.requireNonNull(Histogram.class.getResource(file)).toURI());
+        List<String> text = Files.readAllLines(path, charset);
+
+        System.out.println("Lines = " + text.size());
+
+        System.out.println("Words = " + getWordCount(text));
+
+        System.out.println("Chars = " + getCharacterCount(text));
+    }
+
+    private static int getCharacterCount(List<String> text) {
+        final int[] charCount = {0};
+        text.forEach(element -> {
+            char[] lettersAndDigits = element.toCharArray();
+            charCount[0] += lettersAndDigits.length;
+        });
+        return charCount[0];
+    }
+
+    private static int getWordCount(List<String> text) {
+        final int[] wordCount = {0};
+        text.forEach(element -> {
+            element = element.strip().replaceAll("\\s+", " ");
+            String[] words = element.split(" ");
+            wordCount[0] += words.length;
+        });
+        return wordCount[0];
     }
 }
