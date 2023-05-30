8f7832de1d797638c781dbe20fe5d87e706a68b8
Jjanisruduks <janis.ruduks157@gmail.com>
2023-05-30 12:07:13 +0300

    finished homework, will check for mistakes, improvements later

--- src/main/java/io/codelex/collections/practice/Histogram.java
+++ src/main/java/io/codelex/collections/practice/Histogram.java
@@ -1,11 +1,14 @@
 package io.codelex.collections.practice;
 
+import org.apache.commons.lang3.Range;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 public class Histogram {
     private static final Charset charset = Charset.defaultCharset();
@@ -13,11 +16,45 @@ public class Histogram {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         final String scores = fileContent();
-        System.out.println(scores);
+        String[] numbers = scores.split(" ");
+        createHistogram(numbers);
+    }
+
+    private static void createHistogram(String[] scores) {
+        HashMap<Range<Integer>, String> histogram = new LinkedHashMap<>();
+        int start = 0;
+        int end = 9;
+        for (int i = 0; i < 11; i++) {
+            Range<Integer> range = Range.between(start, end);
+            histogram.put(range, "");
+            start += 10;
+            end += 10;
+        }
+        for (String number : scores) {
+            histogram.forEach((k, v) -> {
+                if (k.contains(Integer.valueOf(number))) {
+                    histogram.put(k, histogram.get(k) + "*");
+                }
+            });
+        }
+
+        displayHistogram(histogram);
+    }
+
+    private static void displayHistogram(HashMap<Range<Integer>, String> histogram) {
+        histogram.forEach((k, v) -> {
+            if (k.contains(1)) {
+                System.out.println("0" + k.getMinimum() + "-" + "0" + k.getMaximum() + ": " + v);
+            } else if (k.contains(100)) {
+                System.out.println(k.getMinimum() + ": " + v);
+            } else {
+                System.out.println(k.getMinimum() + "-" + k.getMaximum() + ": " + v);
+            }
+        });
     }
 
     private static String fileContent() throws URISyntaxException, IOException {
-        final Path path = Paths.get(Histogram.class.getResource(file).toURI());
+        final Path path = Paths.get(Objects.requireNonNull(Histogram.class.getResource(file)).toURI());
         return Files.readAllLines(path, charset).stream()
                 .findFirst()
                 .orElseThrow(IllegalStateException::new);
