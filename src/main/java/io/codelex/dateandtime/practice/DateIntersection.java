8406c6a3b5a58e2962fd9edac9e46e1d35d4928c
Jjanisruduks <janis.ruduks157@gmail.com>
2023-06-07 21:45:57 +0300

    fixed datePeriod and added 1 additional test case

--- src/main/java/io/codelex/dateandtime/practice/DateIntersection.java
+++ src/main/java/io/codelex/dateandtime/practice/DateIntersection.java
@@ -3,7 +3,11 @@ package io.codelex.dateandtime.practice;
 import java.time.LocalDate;
 
 public class DateIntersection {
+
     public static void main(String[] args) {
+
+        // This produced pain in my intersection
+        // Should return overlaps
         LocalDate firstStart = LocalDate.of(2022, 1, 1);
         LocalDate firstEnd = LocalDate.of(2022, 1, 15);
         DatePeriod firstPeriod = new DatePeriod(firstStart, firstEnd);
@@ -12,9 +16,28 @@ public class DateIntersection {
         LocalDate secondEnd = LocalDate.of(2022, 1, 25);
         DatePeriod secondPeriod = new DatePeriod(secondStart, secondEnd);
 
-        // this produced pain in my intersection
-        DatePeriod overlap = firstPeriod.intersection(secondPeriod);
-        System.out.println(overlap.getFirstPeriod());
-        System.out.println(overlap.getSecondPeriod());
+        overlap(firstPeriod.intersection(secondPeriod));
+
+        // Should not return cuz date months don't match.
+        LocalDate firstStart1 = LocalDate.of(2023, 4, 1);
+        LocalDate firstEnd1 = LocalDate.of(2023, 4, 15);
+        DatePeriod firstPeriod1 = new DatePeriod(firstStart1, firstEnd1);
+
+        LocalDate secondStart1 = LocalDate.of(2023, 5, 10);
+        LocalDate secondEnd1 = LocalDate.of(2023, 5, 25);
+        DatePeriod secondPeriod1 = new DatePeriod(secondStart1, secondEnd1);
+
+        overlap(firstPeriod1.intersection(secondPeriod1));
+
+    }
+
+    private static void overlap(DatePeriod period) {
+        if (period == null) {
+            System.out.println("Doesn't overlap.");
+        } else {
+            System.out.println("Overlaps");
+            System.out.println(period.getFirstPeriod());
+            System.out.println(period.getSecondPeriod());
+        }
     }
 }
--- src/main/java/io/codelex/dateandtime/practice/DatePeriod.java
+++ src/main/java/io/codelex/dateandtime/practice/DatePeriod.java
@@ -3,9 +3,9 @@ package io.codelex.dateandtime.practice;
 import java.time.LocalDate;
 
 public class DatePeriod { // what is record?
+
     private final LocalDate firstPeriod;
     private final LocalDate secondPeriod;
-
 
     public DatePeriod(LocalDate firstPeriod, LocalDate secondPeriod) {
         this.firstPeriod = firstPeriod;
@@ -13,7 +13,7 @@ public class DatePeriod { // what is record?
     }
 
     public DatePeriod intersection(DatePeriod period) {
-        if (!doOverlap(period)) {
+        if (doOverlap(period)) {
             return new DatePeriod(getOverlapStart(period), getOverlapEnd(period));
         } else {
             return null;
@@ -37,9 +37,8 @@ public class DatePeriod { // what is record?
     }
 
     private boolean doOverlap(DatePeriod period) {
-        LocalDate firstPeriodStart = period.getFirstPeriod();
-        LocalDate firstPeriodEnd = period.getSecondPeriod();
-        return !firstPeriodStart.isAfter(getFirstPeriod()) && !firstPeriodEnd.isAfter(getSecondPeriod());
+        return !(period.getFirstPeriod().isAfter(this.secondPeriod)
+                || period.getSecondPeriod().isBefore(this.firstPeriod));
     }
 
     public LocalDate getFirstPeriod() {
