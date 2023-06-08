6b825b14bd9a22fbd9ee054b8120b35b5f95bb2d
Jjanisruduks <janis.ruduks157@gmail.com>
2023-06-08 22:32:35 +0300

    removed duplicate code, created constants of url's and fixed Optional nested stream

--- src/main/java/io/codelex/custom/weather/GetWeatherApp.java
+++ src/main/java/io/codelex/custom/weather/GetWeatherApp.java
@@ -2,67 +2,52 @@ package io.codelex.custom.weather;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.codelex.custom.prettifyconsole.PrettifyOutput;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 
 public class GetWeatherApp {
+
+    private static final String IP_API = "https://api.ipify.org/?format=json";
+    private static final String LOCATION_API = "http://ip-api.com/json/";
+    private static final String LOCATION_API_QUERY = "?fields=city,lat,lon";
+    private static final String WEATHER_API = "https://api.open-meteo.com/v1/forecast?latitude=";
+    private static final String WEATHER_API_LON = "&longitude=";
+    private static final String WEATHER_API_QUERY = "&current_weather=true";
+
     public static void main(String[] args) {
 
         System.out.println("Trying to get your ip address...");
-        getIPResponse().ifPresent(ipData -> {
-            System.out.println("...Got your ip! " + ipData.getIp());
-
-            System.out.println("Trying to get your location...");
-            getLocationResponse(ipData.getIp()).ifPresent(locationData -> {
-                System.out.println("...Got ip location! " + locationData.getCity());
-
-                System.out.println("Trying to get weather data...");
-                getWeatherResponse(locationData.getLat(), locationData.getLon()).ifPresent(weatherData -> {
-
+        getAPIResponse(IP_API, IPResponse.class)
+                .flatMap(ipData -> {
+                    System.out.println("...Got your ip! " + ipData.getIp());
+                    System.out.println("Trying to get your location...");
+                    return getAPIResponse(LOCATION_API + ipData.getIp() + LOCATION_API_QUERY, LocationResponse.class);
+                })
+                .flatMap(locationData -> {
+                    System.out.println("...Got ip location! " + locationData.getCity());
+                    System.out.println("Trying to get weather data...");
+                    String weatherUrl = WEATHER_API + locationData.getLat()
+                            + WEATHER_API_LON + locationData.getLon()
+                            + WEATHER_API_QUERY;
+                    return getAPIResponse(weatherUrl, WeatherResponse.class);
+                })
+                .ifPresent(weatherData -> {
                     System.out.println("...Got weather data!");
-                    System.out.println("Weather today in "
-                            + locationData.getCity() + " is "
-                            + weatherData.getTemperature() + "°C"
-                    );
+                    System.out.println("Weather today is " + weatherData.getTemperature() + "°C");
                 });
-            });
-        });
-    }
-
-
-    private static Optional<IPResponse> getIPResponse() {
-        String url = ("https://api.ipify.org/?format=json");
-        return getAPIResponse(url, IPResponse.class);
-    }
-
-    private static Optional<LocationResponse> getLocationResponse(String userIP) {
-        String url = "http://ip-api.com/json/" + userIP + "?fields=city,lat,lon";
-        return getAPIResponse(url, LocationResponse.class);
-    }
-
-    private static Optional<WeatherResponse> getWeatherResponse(String latitude, String longitude) {
-        String url = "https://api.open-meteo.com/v1/forecast?latitude="
-                + latitude
-                + "&longitude=" + longitude
-                + "&current_weather=true";
-        return getAPIResponse(url, WeatherResponse.class);
     }
 
     private static <T> Optional<T> getAPIResponse(String url, Class<T> response) {
+        ObjectMapper mapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         try {
-            return Optional.ofNullable(getObjectMapper()
+            return Optional.ofNullable(mapper
                     .readValue(new URL(url), response));
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
         return Optional.empty();
-    }
-
-    private static ObjectMapper getObjectMapper() {
-        return new ObjectMapper()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 }
