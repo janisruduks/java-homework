4ea3dada84d66539829493e534b9ebe16a08cf4f
Jjanisruduks <janis.ruduks157@gmail.com>
2023-06-10 18:26:02 +0300

    added way to find maximum number of searches needed with log2, and cleaned code a bit

--- src/main/java/io/codelex/custom/binaryguessgame/BinaryGuessGame.java
+++ src/main/java/io/codelex/custom/binaryguessgame/BinaryGuessGame.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 
 public class BinaryGuessGame {
 
+    private static final double MAX_NUMBER = 10000.0;
+    private static final int NUMBER_OF_GUESSES = log2((int) MAX_NUMBER);
+
     public static void main(String[] args) {
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Welcome to number guessing game!");
         System.out.println("YES! Ladies and gentlemen I will guess your number!");
-        System.out.println("Choose number between 1-10000");
-        System.out.println("Rules are, I will have 15 guesses and you have to tell me");
+        System.out.println("Choose number between 1-" + (int) MAX_NUMBER);
+        System.out.println("Rules are, I will have " + NUMBER_OF_GUESSES + " guesses and you have to tell me");
         System.out.println("if it's less or higher than I chose");
         System.out.println("type 'chosen' when you chose your number: ");
 
@@ -26,10 +29,10 @@ public class BinaryGuessGame {
 
     private static void guessTheNumber(Scanner keyboard) {
 
-        double magicNumber = 10000.0 / 2;
+        double magicNumber = MAX_NUMBER / 2;
         double binaryMagic = magicNumber;
 
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < NUMBER_OF_GUESSES; i++) {
             System.out.println("Is your number higher 'h' or lower 'l' than " + Math.round(binaryMagic));
             System.out.println("Or if I guessed type 'CHAD'");
             String answer = keyboard.next().toLowerCase();
@@ -51,5 +54,10 @@ public class BinaryGuessGame {
         for (int i = 0; i < 100; i++) {
             System.out.println(message);
         }
+    }
+
+    // Calculates maximum number of searches in perfectly balanced binary tree.
+    private static int log2(int n) {
+        return (int) Math.ceil(Math.log(n) / Math.log(2));
     }
 }
