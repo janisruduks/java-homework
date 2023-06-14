21b6dd214de9fe56844f8a4bea405a9987d0fb7e
Jjanisruduks <janis.ruduks157@gmail.com>
2023-06-14 15:19:11 +0300

    moved N00bTerm to seperate repository and added sockets project

---
+++ src/main/java/io/codelex/custom/sockets/twoclientserver/Client.java
@@ -0,0 +1,35 @@
+package io.codelex.custom.sockets.twoclientserver;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = new Socket("127.0.0.1", 5000);
+        System.out.println("connected");
+
+        DataInputStream input = new DataInputStream(System.in);
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+        String line = "";
+
+        while (!line.equals("End")) {
+            line = input.readLine();
+            output.writeUTF(line);
+        }
+
+        try {
+            input.close();
+            output.close();
+            socket.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
---
+++ src/main/java/io/codelex/custom/sockets/twoclientserver/Server.java
@@ -0,0 +1,60 @@
+package io.codelex.custom.sockets.twoclientserver;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        try {
+            ServerSocket server = new ServerSocket(5000);
+            Runnable user1 = () -> {
+                try {
+                    Socket socket = server.accept();
+                    serverConnection(socket);
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            };
+            Runnable user2 = () -> {
+                try {
+                    Socket socket = server.accept();
+                    serverConnection(socket);
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            };
+
+            new Thread(user1).start();
+            new Thread(user2).start();
+            System.out.println("Waiting for clients...");
+
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void serverConnection(Socket socket) {
+        try {
+            DataInputStream connected = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            String line = "";
+            while (!line.equals("End")) {
+                try {
+                    line = connected.readUTF();
+                    System.out.println(line);
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            System.out.println("Closing connection");
+            socket.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
