05c7ba0825c2cac58a4a0bf5aede3460450f4b1a
Jjanisruduks <janis.ruduks157@gmail.com>
2023-06-15 19:31:11 +0300

    Forgot to add trivia types

--- src/main/java/io/codelex/triviagame/TriviaApi.java
+++ src/main/java/io/codelex/triviagame/TriviaApi.java
@@ -6,13 +6,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
+import java.util.Random;
 
 public class TriviaApi {
 
-    private static final String URL = "http://numbersapi.com/random/trivia?json&?min=0&max=10000000";
+    private static final String URL = "http://numbersapi.com/random/";
+    private static final String URL_QUERY = "?json&?min=0&max=10000000";
 
     public static TriviaQuestion getTriviaQuestion() {
-        Optional<TriviaQuestion> optionalTriviaQuestion = getAPIResponse(URL, TriviaQuestion.class);
+        String finalUrl = URL + getRandomTriviaType() + URL_QUERY;
+        Optional<TriviaQuestion> optionalTriviaQuestion = getAPIResponse(finalUrl, TriviaQuestion.class);
         return optionalTriviaQuestion.orElse(null);
     }
 
@@ -26,5 +29,11 @@ public class TriviaApi {
             System.out.println("Error: " + e.getMessage());
         }
         return Optional.empty();
+    }
+
+    private static String getRandomTriviaType() {
+        Random rng = new Random();
+        String[] triviaTypes = {"trivia", "year", "math"};
+        return triviaTypes[rng.nextInt(3)];
     }
 }
