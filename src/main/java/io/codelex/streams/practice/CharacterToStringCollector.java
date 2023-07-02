76845ba36ee80169753bc9a5a89cdba908388bc0
Jānis Ruduks <37122372+janisruduks@users.noreply.github.com>
2023-07-02 09:58:24 +0300

    Homework-06 (#10)
    * finished streams
    
    * edited datetime test to resolve git issues
    
    * last commit didn't work, removing test from tracking

--- src/main/java/io/codelex/streams/practice/CharacterToStringCollector.java
+++ src/main/java/io/codelex/streams/practice/CharacterToStringCollector.java
@@ -1,35 +1,37 @@
 package io.codelex.streams.practice;
 
+import java.util.Collections;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class CharacterToStringCollector implements Collector<Character, StringBuilder, String> {
     @Override
     public Supplier<StringBuilder> supplier() {
-        return null;
+        return StringBuilder::new;
     }
 
     @Override
     public BiConsumer<StringBuilder, Character> accumulator() {
-        return null;
+        return StringBuilder::append;
     }
 
     @Override
     public BinaryOperator<StringBuilder> combiner() {
-        return null;
+        return StringBuilder::append;
     }
 
     @Override
     public Function<StringBuilder, String> finisher() {
-        return null;
+        return StringBuilder::toString;
     }
 
     @Override
     public Set<Characteristics> characteristics() {
-        return null;
+        return Collections.emptySet();
     }
 }
